560beab98d2cc4f4e1787b4c6b9abdb262309a62
ridemountainpig <lin0905580802@gmail.com>
2023-07-29 17:51:57 +0800

    feat: add count up component

---
+++ components/CountUp.js
@@ -0,0 +1,15 @@
+"use client"
+
+import React, { Component } from 'react'
+import CountUpComponent from 'react-countup';
+
+export default class CountUp extends Component {
+    render() {
+
+        const { endNum } = this.props;
+
+        return (
+            <CountUpComponent end={endNum} separator='' />
+        )
+    }
+}
